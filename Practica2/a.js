ff9f9e04c788ca95271419b46a353d99b34eff70
ANGELMTZ59 <angelmaryinez1@gmail.com>
2025-05-20 16:31:35 -0600

    practica2_ejercicio_a

---
+++ Practica2/a.js
@@ -0,0 +1,11 @@
+const persona = {
+    nombre: "Ivan Isay",
+    edad: 37, 
+    direccion: {
+        ciudad: "Qro",
+        pais: "MX"
+    }
+
+}
+const resultado = `Me llamo ${persona.nombre}, tengo ${persona.edad} años y vivo en ${persona.direccion.ciudad}`;
+console.log(resultado);
